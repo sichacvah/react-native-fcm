b2696a7b12268119e640afcbd877726ba71b3b59
Sichacvah <sichirc@gmail.com>
2016-07-23 18:30:32 +0500

    getApplicationContext

--- android/src/main/java/com/evollu/react/fcm/MessagingService.java
+++ android/src/main/java/com/evollu/react/fcm/MessagingService.java
@@ -64,7 +64,7 @@ public class MessagingService extends FirebaseMessagingService {
       String packageName = getReactApplicationContext().getPackageName();
 
 
-      Intent intent = new Intent(getApplicationContext(), Class.forName(packageName + "." + activityString);
+      Intent intent = new Intent(getApplicationContext(), Class.forName(packageName + "." + activityString));
       intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
       PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), mNotificationId, intent, PendingIntent.FLAG_ONE_SHOT);
 
