3fe5d3fdeaaa422efec9e660fe041adb72fdc711
Sichacvah <sichirc@gmail.com>
2016-07-23 16:01:36 +0500

    fix

--- android/src/main/java/com/evollu/react/fcm/FIRMessagingModule.java
+++ android/src/main/java/com/evollu/react/fcm/FIRMessagingModule.java
@@ -171,12 +171,5 @@ public class FIRMessagingModule extends ReactContextBaseJavaModule implements Li
 
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-    }
 
-    @Override
-    public void onNewIntent(Intent intent){
-        sendEvent("FCMNotificationReceived", parseIntent(intent));
-    }
 }
